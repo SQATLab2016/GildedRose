6d501c35e23cde54ff087707a58349a7fdf6c343
Ana Brime <anabrime@opendeusto.es>
2016-09-08 16:18:08 +0300

    No errors

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -16,7 +16,7 @@ public class GildedRoseTest {
 	//   Item("+5 Dexterity Vest", 10, 20)); x 
 	//   Item("Aged Brie", 2, 0)); x 
 	//   Item("Elixir of the Mongoose", 5, 7)); x 
-	//   Item("Sulfuras, Hand of Ragnaros", 0, 80));
+	//   Item("Sulfuras, Hand of Ragnaros", 0, 80)); x
 	//   Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)); x 
 	//   Item("Conjured Mana Cake", 3, 6)); x 
 
