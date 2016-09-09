eb21200e1ac429924f63f08e588367849f1a557d
dachen <dachen@STUPID-PC715.stnet.local>
2016-09-09 16:51:05 +0300

    project finished

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import fi.oulu.tol.sqat.GildedRose;
@@ -20,6 +21,7 @@ public class GildedRoseTest {
 //   Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 //   Item("Conjured Mana Cake", 3, 6));
 
+	
 	@Test
 	public void testUpdateEndOfDay_AgedBrie_Quality_10_11() {
 		// Arrange
@@ -34,9 +36,53 @@ public class GildedRoseTest {
 		Item itemBrie = items.get(0);
 		assertEquals(11, itemBrie.getQuality());
 	}
-    
+    //my tests
 	@Test
-	public void testUpdateEndOfDay() {
-		fail("Test not implemented");
+	public void testUpdateEndOfDay_AgedBrie_sellIn_0_Quality_10() {
+		//fail("Test not implemented");
+		// Arrange 
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Aged Brie", 0, 10));
+		//Act
+		store.updateEndOfDay();
+		//Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals("quality should be 11", 11, itemBrie.getQuality());
+		
+	}
+	@Test
+	public void testUpdateEndofDay_GeneralItem_Quality_NeverLessThanZero(){
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("General Item", 1, 0));
+		//Act
+		store.updateEndOfDay();
+		//Assert
+		List<Item> items = store.getItems();
+		Item itemGeneral = items.get(0);
+		assertEquals("quality should be 0", 0, itemGeneral.getQuality());
+	}
+	@Test
+	public void testUpdateEndofDay_AgedBrie_Quality_50_50(){
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Aged Brie", 1, 50));
+		//Act
+		store.updateEndOfDay();
+		//Assert
+		List<Item> items = store.getItems();
+		Item agedBrie = items.get(0);
+		assertEquals("quality should be 50", 50, agedBrie.getQuality());
+	}
+	@Test
+	public void testUpdateEndofDay_GeneralItem_SellInLessThanZero_QualityDegradesTwiceAsFast(){
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("General Item", -1, 10));
+		//Act
+		store.updateEndOfDay();
+		//Assert
+		List<Item> items = store.getItems();
+		Item generalItem = items.get(0);
+		assertEquals("quality should be 8", 8, generalItem.getQuality());
+		
 	}
 }
