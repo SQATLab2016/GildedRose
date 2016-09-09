9baca95d288e9b76db7e8508d538ad401d2c07ed
alang <alang@STUPID-PC729.stnet.local>
2016-09-09 16:38:08 +0300

    Project Finished

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -34,6 +34,83 @@ public class GildedRoseTest {
 		Item itemBrie = items.get(0);
 		assertEquals(11, itemBrie.getQuality());
 	}
+	
+	@Test
+	public void testUpdateEndOfDay_BackstagePasses_Quality_10_12() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBackstagePass = items.get(0);
+		assertEquals(12, itemBackstagePass.getQuality());
+	}
+	
+	
+	@Test
+	public void testUpdateEndOfDay_ElixirOfTheMongoose_Quality_7_6() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Elixir of the Mongoose", 5, 7) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemElixirOfTheMongoose = items.get(0);
+		assertEquals("Wrong quality value!" , 6 , itemElixirOfTheMongoose.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_SulfurasHandOfRagnaros_Quality_80_80() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemElixirOfTheMongoose = items.get(0);
+		assertEquals("Wrong quality value!" , 80 , itemElixirOfTheMongoose.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_SulfurasHandOfRagnaros_SellIn_0_0() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Sulfuras, Hand of Ragnaros", 0, 80) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemElixirOfTheMongoose = items.get(0);
+		assertEquals("Wrong SellIn value!" , 0 , itemElixirOfTheMongoose.getSellIn());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_DexterityVest_SellIn_10_9() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("+5 Dexterity Vest", 10, 20) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemDexterityVest = items.get(0);
+		assertEquals("Wrong SellIn value!" , 9 , itemDexterityVest.getSellIn());
+	}
+	
     
 	@Test
 	public void testUpdateEndOfDay() {
