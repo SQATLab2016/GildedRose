fdadc6b8df4f09b4d3098b2655240394ab35eb54
klampela <klampela@STUPID-PC722.stnet.local>
2016-09-09 16:36:14 +0300

    first version

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -35,8 +35,88 @@ public class GildedRoseTest {
 		assertEquals(11, itemBrie.getQuality());
 	}
     
+	
 	@Test
-	public void testUpdateEndOfDay() {
-		fail("Test not implemented");
+	public void testUpdateEndOfDay_DexterityVest_10_20() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("+5 Dexterity Vest", 5, 20) );
+		
+		// Act
+		store.updateEndOfDay();
+		store.updateEndOfDay();
+		store.updateEndOfDay();
+		store.updateEndOfDay();
+		store.updateEndOfDay();
+		store.updateEndOfDay();
+		
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(11, itemBrie.getQuality());
+	}
+    
+	@Test
+	public void testUpdateEndOfDay_Hand_of_Ragnaros() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Hand of Ragnaros", 0, 80) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(11, itemBrie.getQuality());
+	}
+   
+
+	@Test
+	public void testUpdateEndOfDay_Backstage_passes_to_a_TAFKAL80ETC_concert() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(11, itemBrie.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_Conjured_Mana_Cake() {
+		// Arrange
+		GildedRose store = new GildedRose();
+		store.addItem(new Item("Conjured Mana Cake", 3, 6) );
+		
+		// Act
+		store.updateEndOfDay();
+		
+		// Assert
+		List<Item> items = store.getItems();
+		Item itemBrie = items.get(0);
+		assertEquals(11, itemBrie.getQuality());
+	}
+	
+	@Test
+	public void testUpdateEndOfDay_agerBrie_Quality_2_0() {
+		
+		// Arrange
+				GildedRose store = new GildedRose();
+				store.addItem(new Item("Aged Brie", 2, 0) );
+				
+				// Act
+				store.updateEndOfDay();
+				
+				// Assert
+				List<Item> items = store.getItems();
+				Item itemBrie = items.get(0);
+				assertEquals(11, itemBrie.getQuality());
+		
 	}
 }
