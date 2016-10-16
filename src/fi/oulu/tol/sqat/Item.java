1a9430cfb0fb9cfab1303dd8705c39857a9c30c3
JuhaPatsi <juha.patsi.linkedin@gmail.com>
2016-10-16 22:00:18 +0300

    Juha E.E. Pätsi Student number 20750
    GildedRose Test branch refactoring 
    815311A
    Software Quality and Testing

--- src/fi/oulu/tol/sqat/Item.java
+++ src/fi/oulu/tol/sqat/Item.java
@@ -1,35 +1,89 @@
 package fi.oulu.tol.sqat;
 
+/**
+ * Class for storing the data of various items in CildedRose class
+ * <p>
+ * 
+ * @author (refactoring) Juha E.E. Pätsi ID 20750
+ *
+ */
+public class Item
+{
+	String name;
+	int sellIn;
+	int quality;
 
-public class Item {
-    String name;
-    int sellIn; 
-    int quality; 
-    
-    public Item(String name, int sellIn, int quality) {
+	public Item(String name, int sellIn, int quality)
+	{
 		this.setName(name);
 		this.setSellIn(sellIn);
 		this.setQuality(quality);
 	}
-    
+
 	/* Generated getter and setter code */
-    public String getName() {
+	public String getName()
+	{
 		return name;
 	}
-	public void setName(String name) {
+
+	public void setName(String name)
+	{
 		this.name = name;
 	}
-	public int getSellIn() {
+
+	public int getSellIn()
+	{
 		return sellIn;
 	}
-	public void setSellIn(int sellIn) {
+
+	public void setSellIn(int sellIn)
+	{
 		this.sellIn = sellIn;
 	}
-	public int getQuality() {
+
+	public int getQuality()
+	{
 		return quality;
 	}
-	public void setQuality(int quality) {
+
+	public void setQuality(int quality)
+	{
 		this.quality = quality;
 	}
-}
 
+	// Creating helper methods such as decreaseQuality(), increaseQuality(),
+	// decreaseSellIn() in Item class
+	public void decreaseQuality()
+	{
+		quality--;
+	}
+
+	public void increaseQuality()
+	{
+		quality++;
+	}
+
+	public void decreaseSellIn()
+	{
+		sellIn--;
+	}
+
+	// Create methods for checking certain conditions such as isExpired(),
+	// hasReachedMaximumQuality(), hasZeroQuality().
+
+	public boolean isExpired()
+	{
+		return sellIn < 0;
+	}
+
+	public boolean hasReachedMaximumQuality()
+	{
+		return quality >= 50;
+	}
+
+	public boolean hasZeroQuality()
+	{
+		return quality == 0;
+	}
+
+}
