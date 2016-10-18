43ea7087fb328b6e6e35f4940b8f2c5b0a80bb72
Gontzal Ruiz <gontzalrs@hotmail.com>
2016-10-18 19:13:42 +0300

    Finished, Gontzal Ruiz

--- src/fi/oulu/tol/sqat/Item.java
+++ src/fi/oulu/tol/sqat/Item.java
@@ -2,18 +2,18 @@ package fi.oulu.tol.sqat;
 
 
 public class Item {
-    public String name;
+	public String name;
 	public int sellIn; 
-    public int quality; 
-    
-    public Item(String name, int sellIn, int quality) {
+	public int quality; 
+
+	public Item(String name, int sellIn, int quality) {
 		this.setName(name);
 		this.setSellIn(sellIn);
 		this.setQuality(quality);
 	}
-    
+
 	/* Generated getter and setter code */
-    public String getName() {
+	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
@@ -30,6 +30,38 @@ public class Item {
 	}
 	public void setQuality(int quality) {
 		this.quality = quality;
+	}
+
+	public void decreaseQuality(){
+		this.quality = this.quality-1;
+	}
+
+	public void increaseQuality(){
+		this.quality = this.quality+1;
+	}
+
+	public void decreaseSellIn(){
+		this.sellIn = this.sellIn-1;
+	}
+
+	public boolean hasQualityBiggerThanZero(){
+		if(this.quality>0) return true;
+		else return false;
+	}
+	
+	public boolean hasNotReachedMaximumQuality(){
+		if(this.quality<50) return true;
+		else return false;
+	}
+	
+	public boolean isExpired(){
+		if(this.sellIn < 0) return true;
+		else return false;
+	}
+	
+	public boolean sameObject(String a){
+		if(a.equals(this.getName())) return true;
+		else return false;
 	}
 }
 
